06c4ae9c579660d5a2d19ce7eab21111e0af1fc0
PengchongMa <mpengchong@gmail.com>
2022-11-17 11:02:27 -0600

    Update resume.py

--- streamlit_app/app_1/resume.py
+++ streamlit_app/app_1/resume.py
@@ -1,1 +1,183 @@
+import streamlit as st
+from PIL import Image
 
+with open("style.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+#####################
+# Header 
+st.write('''
+# Pengchong Ma, Ph.D.
+##### *Resume* 
+''')
+
+image = Image.open('dp.png')
+st.image(image, width=150)
+
+st.markdown('## Summary', unsafe_allow_html=True)
+st.info('''
+- Bioinformatics Ph.D. student in University of Nebraskra - Lincoln. 
+- Strong verbal and written communication skills.
+
+''')
+
+#####################
+# Navigation
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+  <a class="navbar-brand" target="_blank">Pengchong Ma</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#education">Education</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#research-experience">Research Experience</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#bioinformatics-tools">Bioinformatics Tools</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#social-media">Social Media</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
+
+#####################
+# Custom function for printing text
+def txt(a, b):
+  col1, col2 = st.columns([4,1])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+
+def txt2(a, b):
+  col1, col2 = st.columns([1,4])
+  with col1:
+    st.markdown(f'`{a}`')
+  with col2:
+    st.markdown(b)
+
+def txt3(a, b):
+  col1, col2 = st.columns([1,2])
+  with col1:
+    st.markdown(a)
+  with col2:
+    st.markdown(b)
+  
+def txt4(a, b, c):
+  col1, col2, col3 = st.columns([1.5,2,2])
+  with col1:
+    st.markdown(f'`{a}`')
+  with col2:
+    st.markdown(b)
+  with col3:
+    st.markdown(c)
+
+#####################
+st.markdown('''
+## Education
+''')
+
+txt('**Doctor of Philosophy** (Computer Science), *University of Nebraskra-Lincoln*, USA',
+'2022-present')
+st.markdown('''
+- GPA: `3.80`
+- Research area include`bacteria analytics and tool design`.
+''')
+
+txt('**Master of Science** (Computer Information System), *Boston University*, USA',
+'2019-2021')
+st.markdown('''
+- Data Analytics and Visualization with R, Web Analytics and Mining, Data Mining
+- Database design
+''')
+
+txt('**Bachelors of Arts** (Economics), *San Diego State University*, USA',
+'2015-2018')
+st.markdown('''
+- The National Society of Leadership and Success  
+- Phi Kappa Phi Honor Society  
+''')
+#####################
+st.markdown('''
+## Research Experience
+''')
+
+txt('**Graduate Researcher Assistant**,University of Nebraskra-Lincoln, USA',
+'2022-present')
+st.markdown('''
+- Develop new pipeline for virus, genome, and bacteria for future analytics.
+- Work on alignment tree and build more user input functions to generate graph.
+
+''')
+
+txt('**CAICT Researcher**, CHINA',
+'2021-2021')
+st.markdown('''
+- Research technology risk management topics, participate in the compilation of enterprise digital governance application development reports and officially release them, organize experts in various fields to write artificial intelligence standards and complete the first draft 
+- Integrate the annual reports of joint-stock banks and city commercial banks in recent years, and participate in the compilation of research reports on the effectiveness of the banking industry's digital transformation investment through combing application scenarios, ecological keywords and technology trends. 
+- Evaluated banks and technology company platforms
+''')
+
+txt('**Graduate Research Assistant**, Boston University, USA',
+'2019-2021')
+st.markdown('''
+- Research on various topic on contemporary and emerging information technology and its managements, topic include building Enterprise Workflow, Leveraging M-Commerce and mobile strategies and Technology working together 
+- Assist professor teach UML models topic in master course and grade the assignment 
+''')
+
+#####################
+st.markdown('''
+## Bioinformatics Tools
+''')
+#txt4('ABCpred', 'A web server for the discovery of acetyl- and butyryl-cholinesterase inhibitors', 'http://codes.bio/abcpred/')
+#txt4('AutoWeka', 'An automated data mining software based on Weka', 'http://www.mt.mahidol.ac.th/autoweka/')
+#txt4('ACPred', 'A computational tool for the prediction and analysis of anticancer peptides','http://codes.bio/acpred/')
+#txt4('BioCurator', 'A web server for curating ChEMBL bioactivity data', 'http://codes.bio/biocurator/')
+#txt4('CryoProtect', 'A web server for classifying antifreeze proteins from non-antifreeze proteins','http://codes.bio/cryoprotect/')
+#txt4('ERpred', 'A web server for the prediction of subtype-specific estrogen receptor antagonists', 'http://codes.bio/erpred')
+#txt4('HCVpred', 'A web server for predicting the bioactivity of Hepatitis C virus NS5B inhibitors', 'http://codes.bio/hemopred/')
+#txt4('HemoPred', 'A web server for predicting the hemolytic activity of peptides', 'http://codes.bio/hemopred/')
+#txt4('iQSP', 'A sequence-based tool for the prediction and analysis of quorum sensing peptides', 'http://codes.bio/iqsp/')
+#txt4('Meta-iAVP', 'A sequence-based meta-predictor for improving the prediction of antiviral peptides', 'http://codes.bio/meta-iavp/')
+#txt4('osFP', 'A web server for predicting the oligomeric state of fluorescent proteins', 'http://codes.bio/osfp/')
+#txt4('PAAP', 'A web server for predicting antihypertensive activity of peptides', 'http://codes.bio/paap/')
+#txt4('PepBio', 'A web server for predicting the bioactivity of host defense peptide', 'http://codes.bio/pepbio')
+#txt4('PyBact', 'Open source software written in Python for bacterial identification', 'https://sourceforge.net/projects/pybact/')
+#txt4('TargetAntiAngio', 'A sequence-based tool for the prediction and analysis of anti-angiogenic peptides','http://codes.bio/targetantiangio/')
+#txt4('ThalPred', 'Development of decision model for discriminating Thalassemia trait and Iron deficiency anemia','http://codes.bio/thalpred/')
+#txt4('THPep', 'A web server for predicting tumor homing peptides','http://codes.bio/thpep/')
+
+
+#####################
+st.markdown('''
+## Skills
+''')
+txt3('Programming', '`Python`, `R`, `Linux`')
+txt3('Data processing/wrangling', '`SQL`, `pandas`, `numpy`')
+txt3('Data visualization', '`matplotlib`, `seaborn`, `plotly`, `altair`, `ggplot2`')
+txt3('Machine Learning', '`scikit-learn`')
+txt3('Deep Learning', '`TensorFlow`')
+txt3('Web development', '`Flask`, `HTML`, `CSS`')
+txt3('Model deployment', '`streamlit`, `gradio`, `R Shiny`, `Heroku`, `AWS`, `Digital Ocean`')
+
+#####################
+st.markdown('''
+## Social Media
+''')
+txt2('LinkedIn', 'https://www.linkedin.com/in/pengchong-ma-18bb39135/')
+txt2('GitHub', 'https://github.com/mapengchong')
+txt2('Google Scholar', 'https://scholar.google.com/citations?user=-14gbKwAAAAJ&hl=en')
