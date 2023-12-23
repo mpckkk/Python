430f5959fcdaea2fbfe59c95ce940ea4cf26ad80
PengchongMa <mpengchong@gmail.com>
2023-12-23 04:14:52 -0600

    Update stock.py

--- streamlit_app/app_2/stock.py
+++ streamlit_app/app_2/stock.py
@@ -7,7 +7,7 @@ import numpy as np
 import yfinance as yf
 
 st.title('S&P 500 App')
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
 st.markdown("""
 This app retrieves the list of the **S&P 500** (from Wikipedia) and its corresponding **stock closing price** (year-to-date)!
 * **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
